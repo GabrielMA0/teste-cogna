c1c3cdfd779348fcdeed9fd2a636c5312356cedd
Gabriel Moreira Araújo <moreira.araujo99@gmail.com>
2026-05-05 10:49:05 -0300

    feat: Add logging for base URL and fetch URL in Page component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ import { notFound } from 'next/navigation';
 
 export default async function Page() {
 
+  const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
+  
+  console.log("BASE URL:", baseUrl);
+  console.log("FETCH URL:", `${baseUrl}/graphql`);
+
   const page = await getPage("inicio");
 
   if (!page) return notFound();
